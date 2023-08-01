08841da85e4fa72c7841890fd4d2edae1c819304
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-08-01 15:41:25 +0000

    First commit Project JS Domain Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let arrayCom = generateDomain(pronoun, adj, noun, ".com");
+  generateList(arrayCom, "com");
+  let arrayUs = generateDomain(pronoun, adj, noun, ".us");
+  generateList(arrayUs, "us");
+  let arrayOrg = generateDomain(pronoun, adj, noun, ".org");
+  generateList(arrayOrg, "org");
+  let arrayEdu = generateDomain(pronoun, adj, noun, ".edu");
+  generateList(arrayUs, "edu");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+const generateDomain = (arr1, arr2, arr3, extension) => {
+  let domain = [];
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      for (let z = 0; z < arr3.length; z++) {
+        domain[count] = arr1[i] + arr2[j] + arr3[z] + extension;
+        count++;
+      }
+    }
+  }
+  return domain;
+};
+/*let domainCom = generateDomain(pronoun, adj, noun, ".com");
+let domainUs = generateDomain(pronoun, adj, noun, ".us");
+let domainOrg = generateDomain(pronoun, adj, noun, ".org");
+let domainEdu = generateDomain(pronoun, adj, noun, ".edu");*/
+
+console.log(generateDomain(pronoun, adj, noun, ".com"));
+function generateList(array, domainExtension) {
+  for (let i = 0; i < array.length; i++) {
+    let dom = domainExtension;
+    let list = document.createElement("li");
+    list.innerHTML = array[i];
+    document.getElementById(dom).appendChild(list);
+    document.getElementById(dom).className = "list-group-item";
+  }
+}
